024b6ed531625d116e8c35a912039de1f784f367
Vadim Sobolev <vadsobolev@yandex.ru>
2023-06-01 23:42:42 +0300

    dz_2

---
+++ dz2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int number = ReadInt("Введите число: ");
+int length = NumberLength(number);
+SumNumbers(number, length);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLength(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int length)
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
